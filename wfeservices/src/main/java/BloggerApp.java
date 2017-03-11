058954b9d8d2c1f444b3f2787dfc33f625b0b13b
Konrad Mrozewski <k.mrozewski@kainos.com>
2017-03-11 13:27:15 +0100

    add dropwizard with google guice as IoC

---
+++ wfeservices/src/main/java/BloggerApp.java
@@ -0,0 +1,45 @@
+import configuration.BloggerAppConfiguration;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import ru.vyarus.dropwizard.guice.GuiceBundle;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
+
+public class BloggerApp extends Application<BloggerAppConfiguration> {
+
+	private GuiceBundle<BloggerAppConfiguration> guiceBundle;
+
+	@Override
+	public void initialize(Bootstrap<BloggerAppConfiguration> bootstrap) {
+		guiceBundle = GuiceBundle.<BloggerAppConfiguration>builder()
+				.enableAutoConfig("main") // autoscan
+				.searchCommands()
+				.build();
+
+		bootstrap.addBundle(guiceBundle);
+	}
+
+	@Override
+	public void run(BloggerAppConfiguration configuration, Environment environment) throws Exception {
+		enableCors(environment);
+	}
+
+	public static void main(String[] args) throws Exception {
+		new BloggerApp().run(args);
+	}
+
+	private void enableCors(Environment environment) {
+		final FilterRegistration.Dynamic cors =
+				environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+		cors.setInitParameter("allowedOrigins", "*");
+		cors.setInitParameter("allowedHeaders", "*");
+		cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+		cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+	}
+}
